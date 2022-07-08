ea96f30f83ac33ebcaf9805492b946d6d5e9a193
Sudo Dios <sdidevelop.work@gmail.com>
2022-07-08 19:08:35 +0430

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const ejs = require('ejs')
+
+const port = 80
 
 app.get('/', (req, res) => {
-    res.send();
+    const bozs = ["you","i","ship"]
+    const htmlResult = ejs.renderFile("template.ejs",{persons : bozs})
+    res.send(htmlResult)
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+server.listen(port, () => {
+    console.log(`adams listen on ${port}`);
 });
